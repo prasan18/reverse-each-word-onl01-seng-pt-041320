68414eaf552d2aed718f2505213468845de5c274
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-19 20:35:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,3 @@
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
-def reverse_each_word(sentence1)
-welcome== sentence1.reverse
- puts "#{welcome}"
-end
